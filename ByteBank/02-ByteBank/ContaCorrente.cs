fc696f16bb3d23e6b7bedd59a696535444a99bf6
italo_proconcept <italo.proconcept@gmail.com>
2020-07-13 15:30:58 -0300

    Init C# - Second part - POO Introduction

---
+++ ByteBank/02-ByteBank/ContaCorrente.cs
@@ -0,0 +1,7 @@
+﻿public class ContaCorrente
+{
+    public string Titular;
+    public int NumeroAgencia;
+    public int Numero;
+    public float Saldo;
+}
---
+++ ByteBank/02-ByteBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.Titular = "Ítalo Baciliere";
+
+            Console.ReadLine();
+        }
+    }
+}
