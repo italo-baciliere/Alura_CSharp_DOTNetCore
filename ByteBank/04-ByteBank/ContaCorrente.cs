ae968eb5f38640ba6d3b3609219d227ad4914f3f
italo_proconcept <italo.proconcept@gmail.com>
2020-07-15 12:23:15 -0300

    C# parte2 POO - 04.Namespace

---
+++ ByteBank/04-ByteBank/ContaCorrente.cs
@@ -0,0 +1,65 @@
+﻿public class ContaCorrente
+{
+    public string titular;
+    public int numeroAgencia;
+    public int numero;
+    public double saldo;
+
+    /*-----------------------------
+    |   Função para subtrair um valor da conta da instância
+    |------------------------------
+    */
+    public bool Sacar(double value)  // Utilzar sempre nomes no infinitivo
+    {
+        if (this.saldo <= value) // this -> palavra reservada. O this acessa o saldo desta instancia
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= value;
+            return true;
+        }
+    }
+
+
+    /*-----------------------------
+    |   Função para incrementar um valor da conta da instância
+    |------------------------------
+    |
+    |   Retorna o valor atual do saldo,
+    | apenas para facilitar a exibição ao
+    | usuário.
+    */
+    public double Depositar(double value) 
+    {
+        this.saldo += value;
+
+        return this.saldo;
+    }
+
+
+    /*-----------------------------
+    |   Função para transferir um valor da conta da instância
+    |------------------------------
+    |
+    |   Retorna o valor atual do saldo,
+    | apenas para facilitar a exibição ao
+    | usuário.
+    */
+    public bool Transferir(double value, ContaCorrente contaDestino)
+    {
+
+        if (this.saldo < -100)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= value;
+            contaDestino.Depositar(value);
+
+            return true;
+        }
+    }
+}
---
+++ ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoItalo = new ContaCorrente();
+
+            contaDoItalo.saldo = 300;
+            bool result = contaDoItalo.Sacar(400);
+            Console.WriteLine(result);
+            
+            double resultDepos = contaDoItalo.Depositar(200);
+            Console.WriteLine(resultDepos);
+
+            result = contaDoItalo.Sacar(400);
+            Console.WriteLine(result);
+
+
+            ContaCorrente contaDaGabi = new ContaCorrente();
+            contaDaGabi.titular = "Gabriela";
+            contaDaGabi.saldo = 200;
+            Console.WriteLine($"Saldo conta da Gabi: {contaDaGabi.saldo}");
+            contaDaGabi.Transferir(100, contaDoItalo);
+            Console.WriteLine($"Saldo conta da Gabi: {contaDaGabi.saldo}");
+
+
+            Console.Read();
+        }
+    }
+}
