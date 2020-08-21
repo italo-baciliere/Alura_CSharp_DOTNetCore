6eb0bfa38842117f9396e15757cc635d7fe9c0fe
italo_proconcept <italo.proconcept@gmail.com>
2020-08-21 09:21:13 -0300

    Finished 02.Tratamento de exceções

--- CSharp_04Part_knowingExceptions/ByteBank/Program.cs
+++ CSharp_04Part_knowingExceptions/ByteBank/Program.cs
@@ -8,8 +8,7 @@ namespace ByteBank
         {
             /*----------------------------
              *  CallStack
-             *----------------------------
-             *
+             *----------------------------             
              *  A função das CallStack são:
              * Organizar a execução dos métodos e saber qual
              * método está sendo executado.
@@ -17,27 +16,52 @@ namespace ByteBank
             CurrentAccount conta = new CurrentAccount(32436659, 4987);
 
             try
+            /*
+             * O bloco try pode acompanhar vários blocos catch;
+             */
             {
                 Metodo();
             }
-            catch (DivideByZeroException excecao)
             /*
              * Exceções são objetos! Vamos acessar seus atributos.
              * Para isto, nomeie a referência para a exceção do tipo DivideByZeroException
              *  
              * Agora podemos visitar as propriedades desta exceção
              */
+            catch (DivideByZeroException e)
+            /*
+             * Existe uma convenção para nomear as variáveis que fazem
+             * referência a exceção do bloco catch
+             * 
+             * Para erro usadamos o <e>
+             */
             {
-                Console.WriteLine(excecao.Message);
-                Console.WriteLine(excecao.StackTrace);
-            }                       
-        }
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            catch (Exception e)
+            /*
+             * Exception é a classe pai/base de todas as
+             * exceções no C# e no .NET
+             */
 
+            /*
+             *  Os blocos <catch> são visitados de cima para baixo,
+             * nenhum bloco <catch> após o <catch> (Exception) será
+             * executado, uma vez que esta captura todas as exceções no .NET.
+             */
 
-        static int Dividir(int number, int divisor)
-        {
-            return number / divisor;
+            /* 
+             * O catch (Expetion) captura todas as exceções de C#, então
+             *  se prevocarmos um exceção no bloco try, a mensagem Exceção
+             *  captura e relança! será exibida.
+             */
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
         }
+        
 
 
         static void Metodo()
@@ -49,6 +73,29 @@ namespace ByteBank
         static void TestaDivisao(int divisor)
         {
             Dividir(10, divisor);
+        }
+
+
+
+        static int Dividir(int number, int divisor)
+        {
+            try
+            {
+                return number / divisor;
+            }
+            catch (DivideByZeroException)
+            {                
+                Console.WriteLine($"Erro ao dividir {number} por {divisor}");
+
+                /*----------------------------------------
+                 * throw -> lançando
+                 *---------------------------------------- 
+                 * 
+                 *  Passa a exceção para adiante para alguém tratar ela.
+                 *                  
+                 */
+                throw;                
+            }            
         }
     }
 }
