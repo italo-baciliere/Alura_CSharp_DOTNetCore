174d776627269a738e475950667ef6caa05af945
italo-baciliere <italoluciano57@gmail.com>
2020-10-07 15:52:29 -0300

    c# parte 4 | 06.Finally e using | Class 03 - Liberando recursos

--- CSharp_04Part_knowingExceptions/ByteBank/LeitorDeArquivos.cs
+++ CSharp_04Part_knowingExceptions/ByteBank/LeitorDeArquivos.cs
@@ -5,11 +5,16 @@ using System.Text;
 
 namespace ByteBank
 {
-    class LeitorDeArquivos
+    class LeitorDeArquivos : IDisposable // objetos especificados no bloco using devem implementar a interface IDisposable
+        // Provides a mechanism for releasing unmanaged resources. // Método que libera os recursos
     {
 
 
-
+        /*------------------------------
+        | Variables
+        |--------------------------------
+        *
+        */
         public string Arquivo { get; }
 
 
@@ -18,14 +23,14 @@ namespace ByteBank
         | MÉTODO CONSTRUTOR
         |--------------------------------
         *
+        * Realizar a leitura de um arquivo
+        * 
         */
         public LeitorDeArquivos(string arquivo)
         {
-            Arquivo = arquivo;
+            Arquivo = arquivo; 
 
-            throw new FileNotFoundException();
-
-            //Console.WriteLine("Abrindo arquivo: " + arquivo);
+            throw new FileNotFoundException(); // A exceção que é gerada quando ocorre falha na tentativa de acessar um arquivo que não existe no disco.            
         }
 
 
@@ -35,7 +40,7 @@ namespace ByteBank
             Console.WriteLine("Lendo linha...");
             return "Linha do arquivo";
 
-            throw new IOException();
+            throw new IOException(); // The exception that is thrown when an I/O error occurs
             /* Exceção definidade pelo DOT .NET
              * Eceção do tipo Input e Output
              */
@@ -43,9 +48,24 @@ namespace ByteBank
 
 
 
-        public void Fechar()
+        public void Dispose() // Provides a mechanism for releasing unmanaged resources. // Método que libera os recursos
+            // using confere se a instância é nula e depois chama o dispose
+            // Using sempre chamará o Dispose
         {
             Console.WriteLine("Fechando  arquivo.");
+            throw new NotImplementedException();
         }
     }
 }
+
+
+// ---------------------------------------------------------------------------------------------------
+/*-------------------------
+ * ------------------------
+ * Estudar o using: http://www.macoratti.net/20/02/c_using1.htm
+ * Pesquisar e estudor sobre o idisposable c#
+ */
+
+// Caso onde a referência é nula
+// No caso onde da tudo certo e o Dispode é executado
+// No caso onde a referência não é nula, mas dentro no bloco using conteceu uma exceção.
--- CSharp_04Part_knowingExceptions/ByteBank/Program.cs
+++ CSharp_04Part_knowingExceptions/ByteBank/Program.cs
@@ -12,25 +12,42 @@ namespace ByteBank
 
         private static void CarregarContas()
         {
-            LeitorDeArquivos leitor = null;
-
-            try
+            /*------------------------------------
+            |   using
+            |------------------------------------
+            *   A instrução <using> é usada para usar recursos e ter a certaza que estes recursos serão
+            * liberados no fim do bloco da instrução using.
+            */
+            using (LeitorDeArquivos leitor = new LeitorDeArquivos("LeitorDeContas.txt"))
             {
-                leitor = new LeitorDeArquivos("LeitorDeContas.txt");
-
                 leitor.LerProximaLinha();
-                leitor.LerProximaLinha();
-                leitor.LerProximaLinha();                
             }
-            catch (IOException)
-            {
-                Console.WriteLine("Exceção do tipo IOException capturada");                
-            }
-            finally { // 13:25 Será sempre executado
 
-                if (leitor != null)
-                    leitor.Fechar();
-            }
+
+
+            //try
+            //{
+            //    leitor = new LeitorDeArquivos("LeitorDeContas.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();                
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine("Exceção do tipo IOException capturada");
+
+            //}
+            //finally {
+            //    /* 
+            //     * Server para informar o programador o final da execução da exceção, mesmo que não tenha gerado nem um erro no corpo do tryCatch
+            //     * Será sempre executado, aconteça uma exceção ou não!
+            //     * Será executado tanto com o try e tanto com catch.
+            //    */
+
+            //    if (leitor != null)
+            //        leitor.Fechar();
+            //}
         }
     }
 }
