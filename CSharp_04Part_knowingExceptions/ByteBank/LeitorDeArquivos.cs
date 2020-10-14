e02b5e40b9acbd7d33804abffae67a34c5ab82fe
italo-baciliere <italoluciano57@gmail.com>
2020-10-14 12:09:48 -0300

    C# parte 4 | 06.Finally e using | Finished Course

--- CSharp_04Part_knowingExceptions/ByteBank/LeitorDeArquivos.cs
+++ CSharp_04Part_knowingExceptions/ByteBank/LeitorDeArquivos.cs
@@ -5,8 +5,7 @@ using System.Text;
 
 namespace ByteBank
 {
-    class LeitorDeArquivos : IDisposable // objetos especificados no bloco using devem implementar a interface IDisposable
-        // Provides a mechanism for releasing unmanaged resources. // Método que libera os recursos
+    class LeitorDeArquivos : IDisposable     
     {
 
 
@@ -28,9 +27,7 @@ namespace ByteBank
         */
         public LeitorDeArquivos(string arquivo)
         {
-            Arquivo = arquivo; 
-
-            throw new FileNotFoundException(); // A exceção que é gerada quando ocorre falha na tentativa de acessar um arquivo que não existe no disco.            
+            Arquivo = arquivo;            
         }
 
 
@@ -40,32 +37,14 @@ namespace ByteBank
             Console.WriteLine("Lendo linha...");
             return "Linha do arquivo";
 
-            throw new IOException(); // The exception that is thrown when an I/O error occurs
-            /* Exceção definidade pelo DOT .NET
-             * Eceção do tipo Input e Output
-             */
+            throw new IOException();
         }
 
 
 
-        public void Dispose() // Provides a mechanism for releasing unmanaged resources. // Método que libera os recursos
-            // using confere se a instância é nula e depois chama o dispose
-            // Using sempre chamará o Dispose
+        public void Dispose()            
         {
-            Console.WriteLine("Fechando  arquivo.");
-            throw new NotImplementedException();
+            Console.WriteLine("Fechando  arquivo.");            
         }
     }
 }
-
-
-// ---------------------------------------------------------------------------------------------------
-/*-------------------------
- * ------------------------
- * Estudar o using: http://www.macoratti.net/20/02/c_using1.htm
- * Pesquisar e estudor sobre o idisposable c#
- */
-
-// Caso onde a referência é nula
-// No caso onde da tudo certo e o Dispode é executado
-// No caso onde a referência não é nula, mas dentro no bloco using conteceu uma exceção.
--- CSharp_04Part_knowingExceptions/ByteBank/Program.cs
+++ CSharp_04Part_knowingExceptions/ByteBank/Program.cs
@@ -11,43 +11,11 @@ namespace ByteBank
         }
 
         private static void CarregarContas()
-        {
-            /*------------------------------------
-            |   using
-            |------------------------------------
-            *   A instrução <using> é usada para usar recursos e ter a certaza que estes recursos serão
-            * liberados no fim do bloco da instrução using.
-            */
+        {            
             using (LeitorDeArquivos leitor = new LeitorDeArquivos("LeitorDeContas.txt"))
             {
                 leitor.LerProximaLinha();
-            }
-
-
-
-            //try
-            //{
-            //    leitor = new LeitorDeArquivos("LeitorDeContas.txt");
-
-            //    leitor.LerProximaLinha();
-            //    leitor.LerProximaLinha();
-            //    leitor.LerProximaLinha();                
-            //}
-            //catch (IOException e)
-            //{
-            //    Console.WriteLine("Exceção do tipo IOException capturada");
-
-            //}
-            //finally {
-            //    /* 
-            //     * Server para informar o programador o final da execução da exceção, mesmo que não tenha gerado nem um erro no corpo do tryCatch
-            //     * Será sempre executado, aconteça uma exceção ou não!
-            //     * Será executado tanto com o try e tanto com catch.
-            //    */
-
-            //    if (leitor != null)
-            //        leitor.Fechar();
-            //}
+            }                                             
         }
     }
 }
